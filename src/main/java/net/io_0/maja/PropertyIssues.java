366ff8e0bb7c1c4df69edd52bc7202c4091164fa
Michael Muhr <mm@io-0.net>
2020-01-10 15:56:41 +0100

    Adds property issues convenience features

--- src/main/java/net/io_0/maja/PropertyIssues.java
+++ src/main/java/net/io_0/maja/PropertyIssues.java
@@ -3,6 +3,7 @@ package net.io_0.maja;
 import net.io_0.maja.PropertyIssue.Issue;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
@@ -30,7 +31,32 @@ public class PropertyIssues extends ArrayList<PropertyIssue> {
   @Override
   public String toString() {
     return stream()
-      .map(propertyIssue -> format("%s -> %s", propertyIssue.getPropertyName(), propertyIssue.getIssue()))
+      .map(propertyIssue -> format("%s -> %s (%s)",
+        propertyIssue.getPropertyName(), propertyIssue.getIssue().getCode(), propertyIssue.getIssue().getMessage()
+      ))
       .collect(Collectors.joining("; "));
+  }
+
+  public AdderWithPropertyNamePrefix withPropertyNamePrefix(String prefix) {
+    PropertyIssues self = this;
+    return new AdderWithPropertyNamePrefix() {
+      @Override
+      public boolean add(PropertyIssue propertyIssue) {
+        return self.add(propertyIssue.withPropertyNamePrefix(prefix));
+      }
+
+      @Override
+      public boolean addAll(Collection<? extends PropertyIssue> propertyIssues) {
+        return self.addAll(propertyIssues.stream()
+          .map(propertyIssue -> propertyIssue.withPropertyNamePrefix(prefix))
+          .collect(Collectors.toList())
+        );
+      }
+    };
+  }
+
+  public interface AdderWithPropertyNamePrefix {
+    boolean add(PropertyIssue e);
+    boolean addAll(Collection<? extends PropertyIssue> c);
   }
 }
---
+++ src/test/java/net/io_0/maja/PropertyIssuesTest.java
@@ -0,0 +1,70 @@
+package net.io_0.maja;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Narrative:
+ *   As a property issues API consumer
+ *
+ *   I want convenient ways to create them
+ */
+public class PropertyIssuesTest {
+  /**
+   * Scenario: It should be convenient to create issues
+   */
+  @Test
+  public void creationConvenience() {
+    // Given property issues
+    PropertyIssue pI1 = PropertyIssue.of(propertyName + "1", errorCode + "1", errorMessage + "1");
+    PropertyIssue pI2 = PropertyIssue.of(propertyName + "2", errorCode + "2", errorMessage + "2");
+    PropertyIssue pI3 = PropertyIssue.of(propertyName + "3", errorCode + "3", errorMessage + "3");
+
+    // When they are collected
+    PropertyIssues pIs = PropertyIssues.of(pI1, pI2, pI3);
+
+    // Then they should be present
+    assertTrue(pIs.containsPropertyName(propertyName + "1"));
+    assertTrue(pIs.containsPropertyName(propertyName + "2"));
+    assertTrue(pIs.containsPropertyName(propertyName + "3"));
+    assertEquals(
+      "name1 -> code1 (issue1); " +
+      "name2 -> code2 (issue2); " +
+      "name3 -> code3 (issue3)",
+      pIs.toString()
+    );
+  }
+
+  /**
+   * Scenario: It should be convenient to add issues with a property name prefix
+   */
+  @Test
+  public void addWithPropertyNamePrefixConvenience() {
+    // Given property issues
+    PropertyIssue pI1 = PropertyIssue.of(propertyName + "1", errorCode + "1", errorMessage + "1");
+    PropertyIssue pI2 = PropertyIssue.of(propertyName + "2", errorCode + "2", errorMessage + "2");
+    PropertyIssue pI3 = PropertyIssue.of(propertyName + "3", errorCode + "3", errorMessage + "3");
+
+    // When they are added with property name prefixes
+    PropertyIssues pIs = PropertyIssues.of();
+    pIs.withPropertyNamePrefix("prefix1.").add(pI1);
+    pIs.withPropertyNamePrefix("prefix2.").addAll(PropertyIssues.of(pI2, pI3));
+
+    // Then they should be present
+    assertTrue(pIs.containsPropertyName("prefix1." + propertyName + "1"));
+    assertTrue(pIs.containsPropertyName("prefix2." + propertyName + "2"));
+    assertTrue(pIs.containsPropertyName("prefix2." + propertyName + "3"));
+    assertEquals(
+      "prefix1.name1 -> code1 (issue1); " +
+      "prefix2.name2 -> code2 (issue2); " +
+      "prefix2.name3 -> code3 (issue3)",
+      pIs.toString()
+    );
+  }
+
+  private static String errorCode = "code";
+  private static String errorMessage = "issue";
+  private static String propertyName = "name";
+}
